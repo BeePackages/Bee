4367fc79ddd15eead11eca96e06ea6720c005a8b
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-04-30 22:16:29 +0200

    Various distribution changes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name = "Bee Package Manager (BPM)",
+    name = "BeePM",
     version = '0.1',
     description = "An open-source package manager for GNU/Linux",
     author = "Maeve Garside",
     author_email = "60114762+MolassesLover@users.noreply.github.com",
     url = 'https://github.com/MolassesLover/Bee',
     packages = ['beepm'],
-    ext_modules = cythonize('Source/Main.py'),
+    ext_modules = cythonize('beepm/Main.py'),
 )
